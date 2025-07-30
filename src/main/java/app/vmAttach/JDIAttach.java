69c524c0e48f4dde3e045dc464ef0177a620e7c8
LeoDefossez <leo.defossez@inria.fr>
2025-07-30 10:36:42 +0200

    Adding JDIAttach

---
+++ src/main/java/app/vmAttach/JDIAttach.java
@@ -0,0 +1,54 @@
+package app.vmAttach;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.sun.jdi.Bootstrap;
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.VirtualMachineManager;
+import com.sun.jdi.connect.AttachingConnector;
+import com.sun.jdi.connect.Connector;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+
+public class JDIAttach {
+	
+	/**
+	 * Attach to a java virtual machine located with the given informations
+	 * @param vmConfig all informations needed to find the vm
+	 * @return the Virtual Machine if one found
+	 * @throws IOException                        when unable to attach.
+	 * @throws IllegalConnectorArgumentsException if no connector socket can be used.
+	 */
+	public VirtualMachine attachToJDI(JsonNode vmConfig) throws IllegalConnectorArgumentsException, IOException {
+
+		VirtualMachineManager vmm = Bootstrap.virtualMachineManager();
+		AttachingConnector connector = null;
+
+		// Searching for the connector socket
+		for (AttachingConnector ac : vmm.attachingConnectors()) {
+			if (ac.name().equals("com.sun.jdi.SocketAttach")) {
+				connector = ac;
+				break;
+			}
+		}
+		if (connector == null) {
+			throw new IllegalStateException("No connector socket found");
+		}
+
+		// Configure the arguments
+		Map<String, Connector.Argument> arguments = connector.defaultArguments();
+
+		arguments.get("hostname").setValue(vmConfig.get("host").textValue());
+		arguments.get("port").setValue(vmConfig.get("port").textValue()); // need to correspond to the JVM address
+
+		// Connect to the JVM
+		try {
+			return connector.attach(arguments);
+		} catch (ConnectException e) {
+			throw new IllegalStateException("Connection to the JVM refused, maybe check that the adresses are corresponding");
+		}
+
+	}
+}
